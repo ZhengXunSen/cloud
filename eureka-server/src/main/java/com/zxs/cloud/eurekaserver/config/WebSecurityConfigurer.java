92f9d23e6ea040e448e76a692230ff691fd99d04
bill.zheng <bill.zheng@jfz.com>
2018-11-28 21:22:50 +0800

    加入eureka-server注册验证

---
+++ eureka-server/src/main/java/com/zxs/cloud/eurekaserver/config/WebSecurityConfigurer.java
@@ -0,0 +1,18 @@
+package com.zxs.cloud.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 关闭csrf避免客户端无法验证
+ */
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
